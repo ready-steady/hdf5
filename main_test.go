ada6e4c5297ca18c38585f9c47dba8dcbfa20ac3
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-03-28 11:54:11 +0100

    Merge get_test and put_test into main_test

--- main_test.go
+++ main_test.go
@@ -1,20 +1,70 @@
 package hdf5
 
 import (
+	"fmt"
 	"os"
+	"reflect"
 	"testing"
 
 	"github.com/ready-steady/assert"
 	"github.com/ready-steady/fixture"
 )
 
-func TestOpenClose(t *testing.T) {
+func TestPutGet(t *testing.T) {
 	path := fixture.MakeFile()
 	defer os.Remove(path)
 
-	file, err := Open(path)
+	file, err := Create(path)
 	assert.Success(err, t)
+	for i, o := range fixtureObjects {
+		assert.Success(file.Put(fmt.Sprintf("%c", 'A'+i), o), t)
+	}
+	assert.Success(file.Close(), t)
 
-	err = file.Close()
+	file, err = Open(path)
 	assert.Success(err, t)
+	for i, o := range fixtureObjects {
+		v := reflect.New(reflect.TypeOf(o))
+		p := v.Interface()
+		assert.Success(file.Get(fmt.Sprintf("%c", 'A'+i), p), t)
+		assert.Equal(reflect.Indirect(v).Interface(), o, t)
+	}
+	assert.Success(file.Close(), t)
+}
+
+func TestPutGetWithDimensions(t *testing.T) {
+	path := fixture.MakeFile()
+	defer os.Remove(path)
+
+	data1 := []float64{
+		0.01, 0.02,
+		0.03, 0.04,
+		0.05, 0.06,
+
+		0.07, 0.08,
+		0.09, 0.10,
+		0.11, 0.12,
+
+		0.13, 0.14,
+		0.15, 0.16,
+		0.17, 0.18,
+
+		0.19, 0.20,
+		0.21, 0.22,
+		0.23, 0.24,
+	}
+
+	file, err := Create(path)
+	assert.Success(err, t)
+	assert.Success(file.Put("data", data1, 2, 3, 4), t)
+	assert.Success(file.Close(), t)
+
+	data2 := []float64{}
+
+	file, err = Open(path)
+	assert.Success(err, t)
+	assert.Success(file.Get("data", &data2), t)
+	assert.Success(file.Close(), t)
+
+	assert.Equal(data1, data2, t)
 }
